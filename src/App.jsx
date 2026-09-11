ad88e1c574d922dd96f1f1abeb85cf539f742438
Your GitHub Name <Your-GitHub-Email>
2026-09-11 23:09:22 +0530

    Routing ,Protected Routes and Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./components/Body";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Team from "./components/Team";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
-      <div className="text-2xl font-bold">Hello World</div>
-      <Body />
+      <header className="text-2xl font-bold py-5 bg-black text-white text-center flex">
+        Hello World
+        <nav className="px-20 m-2 w-300 flex justify-between text-lg">
+          <a href="/">Home </a>
+          <a href="/about">About </a>
+          <a href="/team">Team </a>
+          <a href="/login">Login </a>
+        </nav>
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/team" element={<Team />}></Route>
+          </Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1>This about user Balkrishna</h1>
+    </div>
+  );
+};
+
+export default About;
